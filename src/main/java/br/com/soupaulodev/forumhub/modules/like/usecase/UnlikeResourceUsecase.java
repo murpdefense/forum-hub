3f410bffca803b6432446c922c20d24a321a2465
Paulo Marques <paulomarques.rs1@gmail.com>
2024-11-26 10:57:33 -0300

    Refactor: Make LikeRepository field final
    The LikeRepository field in UnlikeResourceUsecase is now
    declared as final to improve code maintainability and
    prevent accidental modification.

--- src/main/java/br/com/soupaulodev/forumhub/modules/like/usecase/UnlikeResourceUsecase.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/like/usecase/UnlikeResourceUsecase.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 @Service
 public class UnlikeResourceUsecase {
 
-    private LikeRepository likeRepository;
+    private final LikeRepository likeRepository;
 
     public UnlikeResourceUsecase(LikeRepository likeRepository) {
         this.likeRepository = likeRepository;
