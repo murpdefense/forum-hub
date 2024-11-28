f34ffc83f5648df95fb30ff67fcb669dcd01190d
Paulo Marques <paulomarques.rs1@gmail.com>
2024-11-28 14:51:15 -0300

    Docs(like): Add documentation to LikeRepository
    Improve documentation for LikeRepository methods, clarifying
    their purpose and parameters.  This enhances code
    readability and maintainability.

--- src/main/java/br/com/soupaulodev/forumhub/modules/like/repository/LikeRepository.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/like/repository/LikeRepository.java
@@ -8,7 +8,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * Repository interface for managing LikeEntity instances.
+ */
 public interface LikeRepository extends JpaRepository<LikeEntity, UUID> {
 
+    /**
+     * Finds a LikeEntity by the given user and topic.
+     *
+     * @param userId the user who liked the topic
+     * @param topicId the topic that was liked
+     * @return an Optional containing the found LikeEntity, or empty if not found
+     */
     Optional<LikeEntity> findByUserAndTopic(UserEntity userId, TopicEntity topicId);
 }
