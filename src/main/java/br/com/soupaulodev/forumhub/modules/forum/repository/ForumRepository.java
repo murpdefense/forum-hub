65cb1d9a500569d0bcaa3f28f0c9af8c3ccf139f
Paulo Marques <paulomarques.rs1@gmail.com>
2024-11-28 14:56:47 -0300

    docs(forum): Improve ForumRepository with Javadoc
    Add Javadoc documentation to the `ForumRepository` class and its
    methods to improve code clarity and understanding.

--- src/main/java/br/com/soupaulodev/forumhub/modules/forum/repository/ForumRepository.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/forum/repository/ForumRepository.java
@@ -9,9 +9,34 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * Repository interface for managing ForumEntity instances.
+ */
 @Repository
 public interface ForumRepository extends JpaRepository<ForumEntity, UUID> {
+
+    /**
+     * Finds a forum by its name.
+     *
+     * @param name the name of the forum
+     * @return an Optional containing the found ForumEntity, or empty if not found
+     */
     Optional<ForumEntity> findByName(String name);
+
+    /**
+     * Finds all forums with pagination support.
+     *
+     * @param pageable the pagination information
+     * @return a list of ForumEntity instances
+     */
     List<ForumEntity> findAllBy(Pageable pageable);
+
+    /**
+     * Finds all forums whose names contain the specified string, ordered by creation date in descending order.
+     *
+     * @param name the string to search for in forum names
+     * @param pageable the pagination information
+     * @return a list of ForumEntity instances
+     */
     List<ForumEntity> findAllByNameContainingOrderByCreatedAtDesc(String name, Pageable pageable);
 }
