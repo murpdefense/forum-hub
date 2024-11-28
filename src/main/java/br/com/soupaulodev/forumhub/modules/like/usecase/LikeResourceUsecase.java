92b0314b9316bf7d9c90ed7a6a2b8d5880beb092
Paulo Marques <paulomarques.rs1@gmail.com>
2024-11-28 14:52:04 -0300

    docs(like): Improve documentation for like use cases
    Add Javadoc documentation to `LikeResourceUsecase` and
    `UnlikeResourceUsecase` classes, clarifying their purpose,
    parameters, and potential exceptions.

--- src/main/java/br/com/soupaulodev/forumhub/modules/like/usecase/LikeResourceUsecase.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/like/usecase/LikeResourceUsecase.java
@@ -13,6 +13,9 @@ import br.com.soupaulodev.forumhub.modules.user.entity.UserEntity;
 import br.com.soupaulodev.forumhub.modules.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class for managing like resources.
+ */
 @Service
 public class LikeResourceUsecase {
 
@@ -20,6 +23,13 @@ public class LikeResourceUsecase {
     private final UserRepository userRepository;
     private final TopicRepository topicRepository;
 
+    /**
+     * Constructs a new LikeResourceUsecase with the specified repositories.
+     *
+     * @param likeRepository the repository for managing likes
+     * @param userRepository the repository for managing users
+     * @param topicRepository the repository for managing topics
+     */
     public LikeResourceUsecase(LikeRepository likeRepository,
                                UserRepository userRepository,
                                TopicRepository topicRepository) {
@@ -28,6 +38,14 @@ public class LikeResourceUsecase {
         this.topicRepository = topicRepository;
     }
 
+    /**
+     * Executes the use case for liking a topic.
+     *
+     * @param requestDTO the data transfer object containing the like request data
+     * @throws UserNotFoundException if the user is not found
+     * @throws TopicNotFoundException if the topic is not found
+     * @throws LikeAlreadyExistsException if the like already exists
+     */
     public void execute(LikeRequestDTO requestDTO) {
 
         UserEntity user = userRepository.findById(requestDTO.getUserId())
