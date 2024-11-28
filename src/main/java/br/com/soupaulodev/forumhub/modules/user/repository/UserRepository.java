078a4faed3456405aa2129d7a0bc40e5a7091438
Paulo Marques <paulomarques.rs1@gmail.com>
2024-11-28 13:15:33 -0300

    feat(user): Add user search methods to UserRepository
    Add methods to find users by username and email, and by
    name or username.  This enhances user retrieval
    capabilities.

--- src/main/java/br/com/soupaulodev/forumhub/modules/user/repository/UserRepository.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/user/repository/UserRepository.java
@@ -8,8 +8,28 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * Repository interface for UserEntity.
+ * Extends JpaRepository to provide CRUD operations for UserEntity.
+ */
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+    /**
+     * Finds a user by their username and email.
+     *
+     * @param username the username of the user
+     * @param email the email of the user
+     * @return an Optional containing the found UserEntity, or empty if no user is found
+     */
     Optional<UserEntity> findByUsernameAndEmail(String username, String email);
+
+    /**
+     * Finds a user by their name or username.
+     *
+     * @param name the name of the user
+     * @param username the username of the user
+     * @return an Optional containing the found UserEntity, or empty if no user is found
+     */
     Optional<UserEntity> findByNameOrUsername(String name, String username);
 }
