69e8104934c548f0855fb61c416f6edf56acdc5a
Paulo Marques <paulomarques.rs1@gmail.com>
2024-11-28 11:19:44 -0300

    refactor(like): Remove unused UpdateTimestamp annotation
    This commit removes the unused `@UpdateTimestamp` annotation from the `LikeEntity` class.  This simplifies the entity and improves code readability.

--- src/main/java/br/com/soupaulodev/forumhub/modules/like/entity/LikeEntity.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/like/entity/LikeEntity.java
@@ -6,7 +6,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.io.Serial;
 import java.io.Serializable;
