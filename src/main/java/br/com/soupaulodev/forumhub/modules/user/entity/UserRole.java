4264fc8846c6c7eb68725eb888ad04e825cd0545
Paulo Marques <paulomarques.rs1@gmail.com>
2024-11-28 13:04:39 -0300

    docs(user): Add documentation to UserRole enum
    Clarify the roles (ADMIN, MODERATOR, USER) within the
    UserRole enum by adding detailed descriptions for each.

--- src/main/java/br/com/soupaulodev/forumhub/modules/user/entity/UserRole.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/user/entity/UserRole.java
@@ -1,7 +1,21 @@
 package br.com.soupaulodev.forumhub.modules.user.entity;
 
+/**
+ * Forums the user is participating in.
+ */
 public enum UserRole {
+    /**
+     * Role for administrative users with full access.
+     */
     ADMIN,
+
+    /**
+     * Role for users who can moderate content.
+     */
     MODERATOR,
+
+    /**
+     * Role for regular users with standard access.
+     */
     USER
 }
