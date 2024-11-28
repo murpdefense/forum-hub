f577750b10882768073e44620fa0004f99f53261
Paulo Marques <paulomarques.rs1@gmail.com>
2024-11-28 14:50:38 -0300

    Docs(like): Improve LikeMapper Javadoc
    Add Javadoc documentation to the `LikeMapper` class, clarifying
    the purpose and parameters of its methods.This improves code
    readability and maintainability.

--- src/main/java/br/com/soupaulodev/forumhub/modules/like/mapper/LikeMapper.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/like/mapper/LikeMapper.java
@@ -7,12 +7,28 @@ import br.com.soupaulodev.forumhub.modules.topic.mapper.TopicMapper;
 import br.com.soupaulodev.forumhub.modules.user.entity.UserEntity;
 import br.com.soupaulodev.forumhub.modules.user.mapper.UserMapper;
 
+/**
+ * Mapper class for converting between LikeEntity and LikeResponseDTO.
+ */
 public class LikeMapper {
 
+    /**
+     * Converts the given user and topic to a LikeEntity.
+     *
+     * @param user the user who liked the topic
+     * @param topic the topic that was liked
+     * @return the created LikeEntity
+     */
     public static LikeEntity toEntity(UserEntity user, TopicEntity topic) {
         return new LikeEntity(user, topic);
     }
 
+    /**
+     * Converts the given LikeEntity to a LikeResponseDTO.
+     *
+     * @param likeEntity the LikeEntity to convert
+     * @return the created LikeResponseDTO
+     */
     public static LikeResponseDTO toResponseDTO(LikeEntity likeEntity) {
         return new LikeResponseDTO(
                 likeEntity.getId(),
