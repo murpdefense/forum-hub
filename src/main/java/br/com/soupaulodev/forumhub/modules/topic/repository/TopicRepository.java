b4e39bbb93da41a7aa0a1560211f85d1e1f9d912
Paulo Marques <paulomarques.rs1@gmail.com>
2024-11-28 14:42:05 -0300

    Docs(topic): Add Javadoc documentation to TopicRepository
    Improved code readability and maintainability by adding
    Javadoc documentation to the `TopicRepository` class, clarifying the purpose and parameters of each method.

--- src/main/java/br/com/soupaulodev/forumhub/modules/topic/repository/TopicRepository.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/topic/repository/TopicRepository.java
@@ -8,9 +8,28 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Mapper class for converting between TopicEntity and various DTOs.
+ */
 @Repository
 public interface TopicRepository extends JpaRepository<TopicEntity, UUID> {
+
+    /**
+     * Retrieves a list of topics ordered by creation date in descending order.
+     *
+     * @param pageable the pagination information
+     * @return a list of TopicEntity instances
+     */
     List<TopicEntity> findAllByOrderByCreatedAtDesc(Pageable pageable);
 
+    /**
+     * Retrieves a list of topics where the title or content contains the specified keyword,
+     * ordered by creation date in descending order.
+     *
+     * @param title the keyword to search in the title
+     * @param message the keyword to search in the content
+     * @param pageable the pagination information
+     * @return a list of TopicEntity instances
+     */
     List<TopicEntity> findAllByTitleContainingIgnoreCaseOrContentContainingIgnoreCaseOrderByCreatedAtDesc(String title, String message, Pageable pageable);
 }
