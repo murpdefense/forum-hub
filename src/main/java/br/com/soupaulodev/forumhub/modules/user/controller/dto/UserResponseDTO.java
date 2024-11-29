689b22e23e1f5ed2eca616c2674b8037665d2793
Paulo Marques <paulomarques.rs1@gmail.com>
2024-11-29 10:41:11 -0300

    feat(dto): Add no-args constructor to UserResponseDTO
    This commit adds a no-args constructor to the
    UserResponseDTO class.  This is necessary for
    certain frameworks and libraries to properly
    instantiate the object.

--- src/main/java/br/com/soupaulodev/forumhub/modules/user/controller/dto/UserResponseDTO.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/user/controller/dto/UserResponseDTO.java
@@ -3,6 +3,7 @@ package br.com.soupaulodev.forumhub.modules.user.controller.dto;
 import br.com.soupaulodev.forumhub.modules.user.entity.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -11,6 +12,7 @@ import java.util.UUID;
  * Data Transfer Object for creating a new user.
  */
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class UserResponseDTO {
 
