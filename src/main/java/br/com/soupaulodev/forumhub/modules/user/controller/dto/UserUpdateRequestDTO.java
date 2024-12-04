f35a3dc596be1e549cfa6df9766376d2d9df4f22
Paulo Marques <paulomarques.rs1@gmail.com>
2024-12-04 14:34:26 -0300

    Refactor: Use record for UserUpdateRequestDTO
    Replace the UserUpdateRequestDTO class with a record for
    improved conciseness and readability.  Removed unnecessary
    comments and Lombok annotations.

--- src/main/java/br/com/soupaulodev/forumhub/modules/user/controller/dto/UserUpdateRequestDTO.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/user/controller/dto/UserUpdateRequestDTO.java
@@ -1,49 +1,16 @@
 package br.com.soupaulodev.forumhub.modules.user.controller.dto;
 
-import br.com.soupaulodev.forumhub.modules.user.entity.UserRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
-/**
- * The role of the user.
- */
-@Data
-@AllArgsConstructor
-public class UserUpdateRequestDTO {
+public record UserUpdateRequestDTO (@Size(max = 50)
+                                    String name,
 
-    /**
-     * The name of the user.
-     * Must have a maximum size of 50 characters.
-     */
-    @Size(max = 50)
-    private String name;
+                                    @Size(max = 50)
+                                    String username,
 
-    /**
-     * The username of the user.
-     * Must have a maximum size of 50 characters.
-     */
-    @Size(max = 50)
-    private String username;
+                                    @Email
+                                    String email,
 
-    /**
-     * The email of the user.
-     * Must be a valid email format and have a maximum size of 50 characters.
-     */
-    @Email
-    @Size(max = 50)
-    private String email;
-
-    /**
-     * The password of the user.
-     * Must have a size between 8 and 50 characters.
-     */
-    @Size(min = 8, max = 50)
-    private String password;
-
-    /**
-     * The role of the user.
-     */
-    private UserRole role;
-}
+                                    @Size(min = 8, max = 50)
+                                    String password) {}
