faf47ecf25a38d7909f18e4390971ba237443e69
Paulo Marques <paulomarques.rs1@gmail.com>
2024-12-06 20:00:54 -0300

    Docs(repository): Improve User Repository Javadoc
    Improve Javadoc for UserRepository methods by adding
    more specific types to the parameters and return
    values, enhancing clarity and readability.

--- src/main/java/br/com/soupaulodev/forumhub/modules/user/repository/UserRepository.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/user/repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 
 /**
  * Repository interface for UserEntity.
- * Extends JpaRepository to provide CRUD operations for UserEntity.
+ * Extends {@link JpaRepository} to provide CRUD operations for {@link UserEntity}.
  */
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
@@ -17,34 +17,34 @@ public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     /**
      * Finds a user by their username.
      *
-     * @param username the username of the user
-     * @return an Optional containing the found UserEntity, or empty if no user is found
+     * @param username {@link String} the username of the user
+     * @return an {@link Optional} containing the found {@link UserEntity}, or empty if no user is found
      */
     Optional<UserEntity> findByUsername(String username);
 
     /**
      * Finds a user by their username and email.
      *
-     * @param username the username of the user
-     * @param email the email of the user
-     * @return an Optional containing the found UserEntity, or empty if no user is found
+     * @param username {@link String} the username of the user
+     * @param email {@link String} the email of the user
+     * @return an {@link Optional} containing the found {@link UserEntity}, or empty if no user is found
      */
     Optional<UserEntity> findByUsernameAndEmail(String username, String email);
 
     /**
      * Finds a user by their name or username.
      *
-     * @param name the name of the user
-     * @param username the username of the user
-     * @return an Optional containing the found UserEntity, or empty if no user is found
+     * @param name {@link String} the name of the user
+     * @param username {@link String} the username of the user
+     * @return an {@link Optional} containing the found {@link UserEntity}, or empty if no user is found
      */
     Optional<UserEntity> findByNameOrUsername(String name, String username);
 
     /**
      * Checks if a user with the given username exists.
      *
-     * @param username the username to check
-     * @return true if a user with the given username exists, false otherwise
+     * @param username {@link String}the username to check
+     * @return true {@link Boolean} if a user with the given username exists, false otherwise
      */
     Boolean existsByUsername(String username);
 }
