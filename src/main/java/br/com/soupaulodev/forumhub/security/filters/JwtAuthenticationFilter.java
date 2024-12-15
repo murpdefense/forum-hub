9dfb99ce8ab704aa2ecd2950d1891b60debe157c
Paulo Marques <paulomarques.rs1@gmail.com>
2024-12-15 00:13:34 -0300

    Refactor: Remove unused UserDetails import
    Remove unused import of `UserDetails` from`JwtAuthenticationFilter`. This simplifies the code and
    improves readability.

--- src/main/java/br/com/soupaulodev/forumhub/security/filters/JwtAuthenticationFilter.java
+++ src/main/java/br/com/soupaulodev/forumhub/security/filters/JwtAuthenticationFilter.java
@@ -10,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
