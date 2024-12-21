5b3a372e6d7bfc8a6dd3f6ecde0a176864413468
Paulo Marques <paulomarques.rs1@gmail.com>
2024-12-21 13:50:14 -0300

    feat(topic): Enhance TopicEntity and add TopicHighsEntity
    Improve TopicEntity with detailed Javadoc, equals and
    hashCode methods, and add highs and comments counters.  Add
    TopicHighsEntity to manage topic likes.  Use ULID for
    primary keys in TopicHighsEntity.

--- src/main/java/br/com/soupaulodev/forumhub/modules/topic/entity/TopicEntity.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/topic/entity/TopicEntity.java
@@ -12,16 +12,21 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 /**
  * Entity representing a topic in the forum.
+ * <p>
+ * The {@link TopicEntity} class encapsulates the details of a topic.
+ * </p>
  *
  * @author <a href="https://soupaulodev.com.br">soupaulodev</a>
  */
 @Entity
 @Table(name = "tb_topic")
 public class TopicEntity implements Serializable {
+
     @Serial
     private static final long serialVersionUID = 1L;
 
@@ -34,53 +39,40 @@ public class TopicEntity implements Serializable {
     @Column(nullable = false, length = 500)
     private String content;
 
-    /**
-     * The forum to which the topic belongs.
-     * Must not be null.
-     */
-    @ManyToOne
-    @JoinColumn(name = "forum_id", nullable = false)
-    private ForumEntity forum;
-
-    /**
-     * The creator of the topic.
-     * Must not be null.
-     */
-    @ManyToOne
-    @JoinColumn(name = "creator_id", nullable = false)
-    private UserEntity creator;
-
-    /**
-     * The comments associated with the topic.
-     */
-    @OneToMany(mappedBy = "topic", cascade = CascadeType.ALL, orphanRemoval = true)
-    private final List<CommentEntity> comments = new ArrayList<>();
+    @Column(name = "highs_count")
+    private Long highsCount = 0L;
 
     @Column(name = "comments_count")
     private Long commentsCount = 0L;
 
-    @Column(name = "highs_count")
-    private Long highsCount = 0L;
 
-    /**
-     * The timestamp when the topic was created.
-     * Automatically set when the topic is created.
-     */
+
+    @ManyToOne
+    @JoinColumn(name = "forum_id", nullable = false)
+    private ForumEntity forum;
+
+    @ManyToOne
+    @JoinColumn(name = "creator_id", nullable = false)
+    private UserEntity creator;
+
+    @OneToMany(mappedBy = "topic", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<TopicHighsEntity> highs = new ArrayList<>();
+
+    @OneToMany(mappedBy = "topic", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<CommentEntity> comments = new ArrayList<>();
+
+
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     private Instant createdAt;
 
-    /**
-     * The timestamp when the topic was last updated.
-     * Automatically updated when the topic is modified.
-     */
     @UpdateTimestamp
     @Column(name = "updated_at", nullable = false)
     private Instant updatedAt;
 
     /**
      * Default constructor.
-     * Initializes the topic with a new UUID and the current timestamp.
+     * Initializes the id, createdAt and updatedAt fields.
      */
     public TopicEntity() {
         Instant now = Instant.now();
@@ -91,103 +83,85 @@ public class TopicEntity implements Serializable {
     }
 
     /**
-     * Constructor with title, content, and state.
-     * Initializes the topic with a new UUID and the current timestamp.
+     * Constructs a new TopicEntity with the specified title and content.
+     * Initializes the id, createdAt and updatedAt fields.
      *
      * @param title   the title of the topic
      * @param content the content of the topic
      */
     public TopicEntity(String title, String content) {
-        Instant now = Instant.now();
-
-        this.id = UUID.randomUUID();
+        this();
         this.title = title;
         this.content = content;
-        this.createdAt = now;
-        this.updatedAt = now;
     }
 
     /**
-     * Constructor with title, content, creator, and forum.
-     * Initializes the topic with a new UUID and the current timestamp.
+     * Constructs a new TopicEntity with the specified title, content, creator and forum.
+     * Initializes the id, createdAt and updatedAt fields.
      *
      * @param title   the title of the topic
      * @param content the content of the topic
      * @param creator the creator of the topic
-     * @param forum   the forum to which the topic belongs
+     * @param forum   the forum where the topic was created
      */
     public TopicEntity(String title, String content, UserEntity creator, ForumEntity forum) {
-        this();
-        this.title = title;
-        this.content = content;
+        this(title, content);
         this.forum = forum;
         this.creator = creator;
     }
 
+    /**
+     * Gets the unique identifier of the topic.
+     *
+     * @return the unique identifier of the topic
+     */
     public UUID getId() {
         return id;
     }
 
+    /**
+     * Sets the unique identifier of the topic.
+     *
+     * @param id the unique identifier of the topic
+     */
     public void setId(UUID id) {
         this.id = id;
     }
 
+    /**
+     * Gets the title of the topic.
+     *
+     * @return the title of the topic
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title of the topic.
+     *
+     * @param title the title of the topic
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Gets the content of the topic.
+     *
+     * @return the content of the topic
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * Sets the content of the topic.
+     *
+     * @param content the content of the topic
+     */
     public void setContent(String content) {
         this.content = content;
-    }
-
-    public ForumEntity getForum() {
-        return forum;
-    }
-
-    public void setForum(ForumEntity forum) {
-        if (forum != null && !forum.equals(this.forum)) {
-            this.forum = forum;
-            if (!forum.getTopics().contains(this)) {
-                forum.addTopic(this);
-            }
-        }
-    }
-
-    public UserEntity getCreator() {
-        return creator;
-    }
-
-    public void setCreator(UserEntity creator) {
-        if (creator != null && !creator.equals(this.creator)) {
-            this.creator = creator;
-            if (!creator.getTopics().contains(this)) {
-                creator.addTopic(this);
-            }
-        }
-    }
-
-    public List<CommentEntity> getComments() {
-        return comments;
-    }
-
-    public void incrementComments() {
-        this.commentsCount++;
-    }
-
-    public void decrementComments() {
-        this.commentsCount--;
-    }
-
-    public Long getCommentsCount() {
-        return commentsCount;
     }
 
     /**
@@ -213,19 +187,176 @@ public class TopicEntity implements Serializable {
         this.highsCount--;
     }
 
+    /**
+     * Gets the number of comments count.
+     *
+     * @return the number of comments count.
+     */
+    public Long getCommentsCount() {
+        return commentsCount;
+    }
+
+    /**
+     * Increments the number of comments count.
+     */
+    public void incrementComments() {
+        this.commentsCount++;
+    }
+
+    /**
+     * Decrements the number of comments count.
+     */
+    public void decrementComments() {
+        this.commentsCount--;
+    }
+
+    /**
+     * Gets the forum where the topic was created.
+     *
+     * @return the forum where the topic was created
+     */
+    public ForumEntity getForum() {
+        return forum;
+    }
+
+    /**
+     * Sets the forum where the topic was created.
+     *
+     * @param forum the forum where the topic was created
+     */
+    public void setForum(ForumEntity forum) {
+        if (forum != null && !forum.equals(this.forum)) {
+            this.forum = forum;
+            if (!forum.getTopics().contains(this)) {
+                forum.addTopic(this);
+            }
+        }
+    }
+
+    /**
+     * Gets the creator of the topic.
+     *
+     * @return the creator of the topic
+     */
+    public UserEntity getCreator() {
+        return creator;
+    }
+
+    /**
+     * Sets the creator of the topic.
+     *
+     * @param creator the creator of the topic
+     */
+    public void setCreator(UserEntity creator) {
+        if (creator != null && !creator.equals(this.creator)) {
+            this.creator = creator;
+            if (!creator.getTopics().contains(this)) {
+                creator.addTopic(this);
+            }
+        }
+    }
+
+    /**
+     * Gets the list of highs data of the topic.
+     *
+     * @return the highs of the topic
+     */
+    public List<TopicHighsEntity> getHighs() {
+        return highs;
+    }
+
+    /**
+     * Gets the list of comments data of the topic.
+     *
+     * @return the comments of the topic
+     */
+    public List<CommentEntity> getComments() {
+        return comments;
+    }
+
+    /**
+     * Gets the creation date of the topic.
+     *
+     * @return the creation date of the topic
+     */
     public Instant getCreatedAt() {
         return createdAt;
     }
 
+    /**
+     * Sets the creation date of the topic.
+     *
+     * @param createdAt the creation date of the topic
+     */
     public void setCreatedAt(Instant createdAt) {
         this.createdAt = createdAt;
     }
 
+    /**
+     * Gets the date and time when the topic was last updated.
+     *
+     * @return the date and time when the topic was last updated
+     */
     public Instant getUpdatedAt() {
         return updatedAt;
     }
 
+    /**
+     * Sets the date and time when the topic was last updated.
+     *
+     * @param updatedAt the date and time when the topic was last updated
+     */
     public void setUpdatedAt(Instant updatedAt) {
         this.updatedAt = updatedAt;
+    }
+
+    /**
+     * Returns a string representation of the topic.
+     *
+     * @return a string representation of the topic.
+     */
+    @Override
+    public String toString() {
+        return "TopicEntity{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", highsCount=" + highsCount +
+                ", commentsCount=" + commentsCount +
+                ", forum=" + forum +
+                ", creator=" + creator +
+                ", highs=" + highs +
+                ", comments=" + comments +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+
+    /**
+     * Compares this topic to the specified object.
+     *
+     * @param obj the object to compare this topic against.
+     * @return {@code true} if the object is equal to this topic, {@code false} otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof TopicEntity that)) return false;
+
+        return Objects.equals(this.id, that.id);
+    }
+
+    /**
+     * Returns a hash code value for the TopicEntity object.
+     * <p>
+     * This method generates a hash code based on the topic's ID, ensuring that equal objects (i.e., with the same ID)
+     * have the same hash code. It is used in hashing data structures such as HashMap and HashSet.
+     * </p>
+     *
+     * @return the hash code value of the TopicEntity object.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- src/main/java/br/com/soupaulodev/forumhub/modules/topic/entity/TopicHighsEntity.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/topic/entity/TopicHighsEntity.java
@@ -1,30 +1,199 @@
 package br.com.soupaulodev.forumhub.modules.topic.entity;
 
+import br.com.soupaulodev.forumhub.config.UlidGenerator;
 import br.com.soupaulodev.forumhub.modules.user.entity.UserEntity;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.Instant;
+import java.util.Objects;
+
+/**
+ * Represents a topic high within the system.
+ * <p>
+ * The {@link TopicHighsEntity} class encapsulates the details of a topic high.
+ * </p>
+ *
+ * @author <a href="https://soupaulodev.com.br">soupaulodev</a>
+ */
 @Entity
 @Table(name = "tb_topic_highs")
 public class TopicHighsEntity {
 
-    private Long id;
+    @Id
+    private String id;
+
 
     @ManyToOne
-    @JoinColumn(name = "topic_id")
+    @JoinColumn(name = "topic_id", nullable = false)
     private TopicEntity topic;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private UserEntity user;
+
 
     @CreationTimestamp
     @Column(name="created_at", nullable = false, updatable = false)
-    private Long createdAt;
+    private Instant createdAt;
 
     @UpdateTimestamp
     @Column(name="updated_at", nullable = false)
-    private Long updatedAt;
+    private Instant updatedAt;
 
+    /**
+     * Default constructor.
+     * Initializes the id
+     */
+    public TopicHighsEntity() {
+        Instant now = Instant.now();
+        this.id = UlidGenerator.generate();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    /**
+     * Constructs a new TopicHighsEntity with the specified user and topic.
+     * Initializes the id, createdAt and updatedAt fields.
+     *
+     * @param topic      the topic that was liked
+     * @param user       the user who liked the topic
+     */
+    public TopicHighsEntity(TopicEntity topic, UserEntity user) {
+        this();
+        this.topic = topic;
+        this.user = user;
+    }
+
+    /**
+     * Gets the unique identifier of the topic high.
+
+     * @return the unique identifier of the topic high
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Sets the unique identifier of the topic high.
+     *
+     * @param id the unique identifier of the topic high
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets the user who liked the topic.
+     *
+     * @return the user who liked the topic
+     */
+    public TopicEntity getTopic() {
+        return topic;
+    }
+
+    /**
+     * Sets the user who liked the topic.
+     *
+     * @param topic the user who liked the topic
+     */
+    public void setTopic(TopicEntity topic) {
+        this.topic = topic;
+    }
+
+    /**
+     * Gets the user who liked the topic.
+     *
+     * @return the user who liked the topic
+     */
+    public UserEntity getUser() {
+        return user;
+    }
+
+    /**
+     * Sets the user who liked the topic.
+     *
+     * @param user the user who liked the topic
+     */
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    /**
+     * Gets the date and time when the topic was liked.
+     *
+     * @return the date and time when the topic was liked
+     */
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    /**
+     * Sets the date and time when the topic was liked.
+     *
+     * @param createdAt the date and time when the topic was liked
+     */
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    /**
+     * Gets the date and time when the topic was last updated.
+     *
+     * @return the date and time when the topic was last updated
+     */
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    /**
+     * Sets the date and time when the topic was last updated.
+     *
+     * @param updatedAt the date and time when the topic was last updated
+     */
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    /**
+     * Returns the string representation of the topic high.
+     *
+     * @return the string representation of the topic high
+     */
+    @Override
+    public String toString() {
+        return "TopicHighsEntity{" +
+                "id='" + id + '\'' +
+                ", topic=" + topic +
+                ", user=" + user +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     *
+     * @param obj the reference object with which to compare
+     * @return {@code true} if this object is the same as the obj argument; {@code false} otherwise
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        TopicHighsEntity that = (TopicHighsEntity) obj;
+        return Objects.equals(this.id, that.id);
+    }
+
+    /**
+     * Returns the hash code value for this topic high.
+     *
+     * @return the hash code value for this topic high
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
