693622edaff463e3f3a305c45654fc959f535fd6
Paulo Marques <paulomarques.rs1@gmail.com>
2024-12-21 14:40:31 -0300

    docs(repo): Add Javadoc to ForumHighsRepository
    Add Javadoc documentation to the `ForumHighsRepository`
    interface, clarifying its purpose and extending
    `JpaRepository`.

--- src/main/java/br/com/soupaulodev/forumhub/modules/forum/repository/ForumHighsRepository.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/forum/repository/ForumHighsRepository.java
@@ -7,6 +7,12 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * Repository interface for {@link ForumHighsEntity}.
+ * Extends {@link JpaRepository} to provide CRUD operations for {@link ForumHighsEntity}.
+ *
+ * @author <a href="http://soupaulodev.com.br">soupaulodev</a>
+ */
 @Repository
 public interface ForumHighsRepository extends JpaRepository<ForumHighsEntity, String> {
     Optional<ForumHighsEntity> findByForum_IdAndUser_Id(UUID forumId, UUID userId);
