f1abfd556de4d45a7cdad41da9ba18246f22ac8c
Paulo Marques <paulomarques.rs1@gmail.com>
2024-12-24 09:22:40 -0300

    feat(user): Add authentication check to HighUserUseCase
    Improve HighUserUseCase to handle unauthenticated users.  Add
    UnauthorizedException to indicate when a user is not
    authenticated.  The use case now throws an exception if the
    authenticated user ID is null.

--- src/main/java/br/com/soupaulodev/forumhub/modules/user/usecase/HighUserUseCase.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/user/usecase/HighUserUseCase.java
@@ -1,5 +1,6 @@
 package br.com.soupaulodev.forumhub.modules.user.usecase;
 
+import br.com.soupaulodev.forumhub.modules.exception.usecase.UnauthorizedException;
 import br.com.soupaulodev.forumhub.modules.user.entity.UserEntity;
 import br.com.soupaulodev.forumhub.modules.user.entity.UserHighsEntity;
 import br.com.soupaulodev.forumhub.modules.user.repository.UserHighsRepository;
@@ -8,6 +9,12 @@ import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+/**
+ * Service responsible for handling the logic of "highing" a user.
+ * A user can give a "high" to another user as long as certain conditions are met.
+ *
+ * @author <a href="https://soupaulodev.com.br">soupaulodev</a>
+ */
 @Service
 public class HighUserUseCase {
 
@@ -20,7 +27,22 @@ public class HighUserUseCase {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Executes the logic for "highing" a user.
+     *
+     * @param highedUser the UUID of the user to be "highed"
+     * @param authenticatedUserId the UUID of the authenticated user giving the "high"
+     * @throws IllegalArgumentException if:
+     *         - the user tries to "high" themselves,
+     *         - the "high" has already been given,
+     *         - either user is not found.
+     * @throws UnauthorizedException if the user is not authenticated.
+     */
     public void execute(UUID highedUser, UUID authenticatedUserId) {
+        if (authenticatedUserId == null) {
+            throw new UnauthorizedException("User not authenticated");
+        }
+
         if (highedUser.equals(authenticatedUserId)) {
             throw new IllegalArgumentException("User cannot high himself");
         }
