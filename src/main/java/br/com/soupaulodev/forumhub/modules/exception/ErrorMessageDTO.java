a0e17eb63019f9ddf952d6ba6e26f743c5182f52
Paulo Marques <paulomarques.rs1@gmail.com>
2024-12-24 11:48:55 -0300

    feat(exception): Improve ErrorMessageDTO
    Enhance ErrorMessageDTO with field property and detailed Javadoc.  Add a toString method for better
    debugging.The field property allows specifying
    which field the error relates to.

--- src/main/java/br/com/soupaulodev/forumhub/modules/exception/ErrorMessageDTO.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/exception/ErrorMessageDTO.java
@@ -1,38 +1,58 @@
 package br.com.soupaulodev.forumhub.modules.exception;
 
 /**
- * Data Transfer Object for error messages.
+ * Data Transfer Object representing an error message.
+ *
+ * @author <a href="https://soupaulodev.com.br">soupaulodev</a>
  */
 public class ErrorMessageDTO {
 
-    /**
-     * The error message.
-     */
     private String message;
+    private final String field;
 
     /**
-     * The field associated with the error.
+     * Constructs an ErrorMessageDTO with the specified message and field.
+     *
+     * @param message the error message
+     * @param field   the field associated with the error
      */
-    private String field;
-
     public ErrorMessageDTO(String message, String field) {
         this.message = message;
         this.field = field;
     }
 
+    /**
+     * Retrieves the error message.
+     *
+     * @return the error message
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Sets the error message.
+     *
+     * @param message the error message to set
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * Retrieves the field associated with the error.
+     *
+     * @return the name of the field
+     */
     public String getField() {
         return field;
     }
 
-    public void setField(String field) {
-        this.field = field;
+    @Override
+    public String toString() {
+        return "ErrorMessageDTO{" +
+                "message='" + message + '\'' +
+                ", field='" + field + '\'' +
+                '}';
     }
 }
