896586829a070dce08d5ac7a4a6c9fc3e1cd9f17
Paulo Marques <paulomarques.rs1@gmail.com>
2024-12-24 10:23:47 -0300

    fix(exception): Add constructor to RateLimitExceededException
    Allow specifying a custom message for RateLimitExceededException.  This improves error handling and provides more context to the user when a rate limit is exceeded.

--- src/main/java/br/com/soupaulodev/forumhub/modules/exception/usecase/RateLimitExceededException.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/exception/usecase/RateLimitExceededException.java
@@ -1,6 +1,11 @@
 package br.com.soupaulodev.forumhub.modules.exception.usecase;
 
 public class RateLimitExceededException extends RuntimeException {
+
+    public RateLimitExceededException(String message) {
+        super(message);
+    }
+
     public RateLimitExceededException() {
         super("Rate limit exceeded. Try again later.");
     }
