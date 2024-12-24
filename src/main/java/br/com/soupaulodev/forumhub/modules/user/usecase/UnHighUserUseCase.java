d3355905c3d62e122ad1e1724f753897aa92d8f1
Paulo Marques <paulomarques.rs1@gmail.com>
2024-12-24 09:22:49 -0300

    feat(user): Improve UnHighUserUseCase error handling
    Add more robust error handling to the UnHighUserUseCase.
    The use case now throws UnauthorizedException if the user
    is not authenticated and IllegalArgumentException if the
    user tries to unhigh themselves or if there is no high
    relationship between the users.  Improved clarity of
    exception messages.

--- src/main/java/br/com/soupaulodev/forumhub/modules/user/usecase/UnHighUserUseCase.java
+++ src/main/java/br/com/soupaulodev/forumhub/modules/user/usecase/UnHighUserUseCase.java
@@ -1,12 +1,14 @@
 package br.com.soupaulodev.forumhub.modules.user.usecase;
 
+import br.com.soupaulodev.forumhub.modules.exception.usecase.UnauthorizedException;
 import br.com.soupaulodev.forumhub.modules.user.repository.UserHighsRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 /**
- * Use case to unhigh a user
+ * Service responsible for handling the logic of "unhighing" a user.
+ * Allows a user to remove a previously given "high" to another user.
  *
  * @author <a href="https://soupaulodev.com.br">soupaulodev</a>
  */
@@ -15,27 +17,30 @@ public class UnHighUserUseCase {
 
     private final UserHighsRepository userHighsRepository;
 
-    /**
-     * Constructor
-     *
-     * @param userHighsRepository User highs repository
-     */
     public UnHighUserUseCase(UserHighsRepository userHighsRepository) {
         this.userHighsRepository = userHighsRepository;
     }
 
     /**
-     * Execute the use case
+     * Executes the logic for "unhighing" a user.
      *
-     * @param highedUser          Highed user
-     * @param authenticatedUserId Authenticated user id
+     * @param unHighedUser the UUID of the user to be "unhighed"
+     * @param authenticatedUserId the UUID of the authenticated user performing the "unhigh"
+     * @throws IllegalArgumentException if:
+     *         - the user tries to "unhigh" themselves,
+     *         - there is no "high" relationship between the users.
+     * @throws UnauthorizedException if the user is not authenticated.
      */
-    public void execute(UUID highedUser, UUID authenticatedUserId) {
-        if (highedUser.equals(authenticatedUserId)) {
+    public void execute(UUID unHighedUser, UUID authenticatedUserId) {
+        if (authenticatedUserId == null) {
+            throw new UnauthorizedException("User not authenticated");
+        }
+
+        if (unHighedUser.equals(authenticatedUserId)) {
             throw new IllegalArgumentException("User cannot unhigh himself");
         }
 
-        userHighsRepository.findByHighedUser_IdAndHighingUser_Id(highedUser, authenticatedUserId)
+        userHighsRepository.findByHighedUser_IdAndHighingUser_Id(unHighedUser, authenticatedUserId)
                 .ifPresentOrElse(userHighsRepository::delete, () -> {
                     throw new IllegalArgumentException("User not highed");
                 });
